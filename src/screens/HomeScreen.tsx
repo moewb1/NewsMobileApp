2ee09ad62ba6c8a281955e9b66585d8638543baf
moetassem <moetassem.wehbe.01@gmail.com>
2026-02-25 18:10:46 +0200

    added react-native-safe-area-context

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -3,12 +3,12 @@ import {
   Alert,
   FlatList,
   Linking,
-  SafeAreaView,
   StatusBar,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import {PrimaryButton} from '../components/common/PrimaryButton';
 import {SearchField} from '../components/common/SearchField';
 import {StatusMessage} from '../components/common/StatusMessage';
